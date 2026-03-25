850a814c267d2e2670019ca262f7eaea61218f82
Umberto Novellis <umberto.novellis@gmail.com>
2026-03-25 17:32:00 +0100

    test

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { HlmButtonImports } from '@spartan-ng/helm/button';
+import { HlmInputImports } from '@spartan-ng/helm/input';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [HlmButtonImports, HlmInputImports],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('testSpartans');
